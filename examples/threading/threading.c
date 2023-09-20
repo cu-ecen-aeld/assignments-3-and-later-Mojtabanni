6553d78c7a78373cca07ca4332810da77777a671
Mojtabanni <mojtabanadi1376@gmail.com>
2023-09-20 16:10:48 +0330

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,19 +2,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
+
+pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void* threadfunc(void* thread_param)
 {
-
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep (thread_func_args->obtain_ms*1000);
+    pthread_mutex_lock(thread_func_args->mutex);
+    usleep (thread_func_args->release_ms*1000);
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    return thread_func_args;
 }
 
 
@@ -28,6 +37,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *data;
+    int ret;
+
+    data = calloc(1, sizeof(struct thread_data));
+    if (data == NULL){
+        perror("calloc");
+        return false;
+    }
+
+    data->mutex = mutex;
+    data->obtain_ms = wait_to_obtain_ms;
+    data->release_ms = wait_to_release_ms;
+
+    
+    ret = pthread_create(thread, NULL, threadfunc, (void *) data);
+    if (ret) {
+        errno = ret;
+        perror ("pthread_create");
+        return false;
+    }
+    
+    
+    return true;
 }
 
