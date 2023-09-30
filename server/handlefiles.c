6ffa67bc2bf136606e5ae14eb990b8be4071fa2b
Mojtabanni <mojtabanadi1376@gmail.com>
2023-09-30 10:25:46 +0330

    assignment5-2.3

---
+++ server/handlefiles.c
@@ -0,0 +1,32 @@
+#include "handlefiles.h"
+#include <fcntl.h>
+
+int open_file(char *pathaname)
+{
+    return open(pathaname, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+}
+
+ssize_t write_file(int pfd, const void *buffer, size_t count)
+{
+    return write(pfd, buffer, count);
+}
+
+ssize_t read_file(int pfd, void *buffer, size_t nbytes)
+{
+    return read(pfd, buffer, nbytes);
+}
+
+int close_file(int pfd)
+{
+    return close(pfd);
+}
+
+int clear_file(char *pathname)
+{
+    int pfd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (pfd > -1)
+    {
+        close(pfd);
+    }
+    return pfd;
+}
